8e077ab04339fd7c501af2a35c79db3d1d6c31fa
Sorp <danvuk11@gmail.com>
2021-05-15 00:33:52 +0300

    Init project Add ray

---
+++ Engine3D/Engine3D/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Numerics;
+
+namespace Engine3D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = @"C:\Users\danvu\Documents\GitHubPP\BmpVisualizer\Engine3D\Engine3D\bin\Debug\net5.0\myimg.png";
+            Bitmap b = new Bitmap(1280,720);
+            ConsoleRenderer renderer = new ConsoleRenderer(b);
+            renderer.Draw();
+            b.Save(url,ImageFormat.Png);
+        }
+    }
+}
