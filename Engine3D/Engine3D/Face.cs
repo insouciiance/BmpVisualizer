f082193a7fd8bd9fd6b09f306ca7588a0bf5dbff
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-05-17 21:21:03 +0300

    added coloring

--- Engine3D/Engine3D/Face.cs
+++ Engine3D/Engine3D/Face.cs
@@ -4,26 +4,23 @@ namespace Engine3D
 {
     public struct Face
     {
-        private Vector3 _point1;
-        private Vector3 _point2;
-        private Vector3 _point3;
-
-        public Vector3 Point1 => _point1;
-        public Vector3 Point2 => _point2;
-        public Vector3 Point3 => _point3;
+        private readonly Vector3[] _points;
+        public Vector3[] Points => _points;
 
         public Face(Vector3 p1,Vector3 p2,Vector3 p3)
         {
-            _point1 = p1;
-            _point2 = p2;
-            _point3 = p3;
+            _points = new Vector3[3];
+            _points[0] = p1;
+            _points[1] = p2;
+            _points[2] = p3;
         }
         
         public Face(params Vector3[] points)
         {
-            _point1 = points.Length >= 1 ? points[0] : Vector3.Zero;
-            _point2 = points.Length >= 2 ? points[1] : Vector3.Zero;
-            _point3 = points.Length >= 3 ? points[2] : Vector3.Zero;
+            _points = new Vector3[3];
+            _points[0] = points.Length >= 1 ? points[0] : Vector3.Zero;
+            _points[1] = points.Length >= 2 ? points[1] : Vector3.Zero;
+            _points[2] = points.Length >= 3 ? points[2] : Vector3.Zero;
         }
 
     }
