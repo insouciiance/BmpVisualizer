4be597d582f835dbbd791ca84ebd78aabd14fb90
Sorp <danvuk11@gmail.com>
2021-05-18 17:11:10 +0300

    add helpers

---
+++ Engine3D/Engine3D/Debug/DebugMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+namespace Engine3D.Debug
+{
+    public static class DebugMesh
+    {
+        public static Mesh Cube = new Mesh(new[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 1, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 1, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(1, 0, 1),
+            new Vector3(0, 0, 1),
+        }, new[] {
+            0, 2, 1, //face front
+            0, 3, 2,
+            2, 3, 4, //face top
+            2, 4, 5,
+            1, 2, 5, //face right
+            1, 5, 6,
+            0, 7, 4, //face left
+            0, 4, 3,
+            5, 4, 7, //face back
+            5, 7, 6,
+            0, 6, 7, //face bottom
+            0, 1, 6
+        });
+        public static Mesh Pyramid = new Mesh(new[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 1),
+            new Vector3(1, 0, 0),
+            new Vector3(.5f, 1, .5f),
+        }, new[] {
+            0, 1, 3, //bottom
+            1, 2, 3,
+            1, 4, 0, //face top
+            2, 4, 1,
+            3, 4, 2, //face right
+            0, 4, 3,
+        });
+    }
+}
---
+++ Engine3D/Engine3D/Debug/VectorsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Engine3D.Debug
+{
+    public static class VectorsHelper
+    {
+        public static Vector3 IncidentSphere(this Random rnd)
+            => Vector3.Normalize(
+                new Vector3( 
+                    (float) rnd.NextDouble(-1,1),
+                    (float) rnd.NextDouble(-1,1),
+                    (float) rnd.NextDouble(-1,1)
+                    )
+                );
+        public static double NextDouble(this Random rnd,double minimum, double maximum)
+        {
+            return rnd.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
