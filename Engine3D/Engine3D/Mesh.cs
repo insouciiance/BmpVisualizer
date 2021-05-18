d639c1589b7fae1ee1fec45f9af368baeb8a15c2
Sorp <danvuk11@gmail.com>
2021-05-18 17:07:56 +0300

    added new way of constructing mesh

--- Engine3D/Engine3D/Mesh.cs
+++ Engine3D/Engine3D/Mesh.cs
@@ -1,4 +1,6 @@
-﻿namespace Engine3D
+﻿using System.Numerics;
+
+namespace Engine3D
 {
     public class Mesh
     {
@@ -8,6 +10,15 @@
         public Mesh(Face[] faces)
         {
             this.faces = faces;
+        }
+
+        public Mesh(Vector3[] vertices, int[] triangles)
+        {
+            faces = new Face[triangles.Length / 3];
+            for (int i = 0; i <= triangles.Length - 3; i+=3)
+            {
+                faces[i / 3] = new Face(vertices[triangles[i]], vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+            }
         }
     }
 }
